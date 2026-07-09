fd9c686bb6e9c95601eff5b8d5c9d7c7bb35f9bb
Ambedkarpublicschool <apsmadhopur2024@gmail.com>
2026-07-09 19:28:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-export const CONFIG = {
+export const CONFIG={
 
-    MODEL_URL:
-    "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.task",
+MODEL_URL:
+"https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.task"
 
-    runningMode:"VIDEO",
-
-    maxFaces:1,
-
-    scoreThreshold:0.60
-
-};
+}
