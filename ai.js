d5d91d5735d3b316c88a3a87006dcd1c6112ee57
Ambedkarpublicschool <apsmadhopur2024@gmail.com>
2026-07-09 19:24:33 +0530

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,40 @@
+import { CONFIG } from "./config.js";
 
+let detector;
+
+export async function initAI(){
+
+    console.log("Loading AI...");
+
+    const vision =
+    await FilesetResolver.forVisionTasks(
+
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+
+    );
+
+    detector =
+    await FaceDetector.createFromOptions(
+
+        vision,
+
+        {
+
+            baseOptions:{
+
+                modelAssetPath:CONFIG.MODEL_URL
+
+            },
+
+            runningMode:CONFIG.runningMode,
+
+            minDetectionConfidence:
+            CONFIG.scoreThreshold
+
+        }
+
+    );
+
+    console.log("AI Ready");
+
+}
